ab8e6088126474c50394eca20caece91d4349fc2
Sam-uch <borodiasemilya@gmail.com>
2026-06-18 23:53:57 +0300

    Update URL pattern for OrderDetailView

--- library/order/urls.py
+++ library/order/urls.py
@@ -3,5 +3,5 @@ from .views import OrderListView, OrderDetailView
 
 urlpatterns = [
     path('', OrderListView.as_view()),
-    path('<int:order_id>/', OrderDetailView.as_view()),
+    path('user/<int:user_id>/order/<int:order_id>/', OrderDetailView.as_view()),
 ]
